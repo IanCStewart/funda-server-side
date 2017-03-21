3cb43cd7bd991b5e6e60a524e91e2731c1c08d70
iancstewart <ian.c.stewart@icloud.com>
2017-03-21 13:10:45 +0100

    :sparkles: Added server side rendering api-response.

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const art = require('ascii-art');
+const fetch = require('node-fetch');
+const mainHtml = require('./lib/main-html');
+
+require('dotenv').config();
+
+const fundaKey = process.env.FUNDA_KEY;
+
+if (!fundaKey) {
+  throw new Error('Missing `FUNDA_KEY` in env.');
+}
+
+const fundaEndpoint = (type, address, options, page, size) => `http://funda.kyrandia.nl/feeds/Aanbod.svc/json/${fundaKey}/?type=${type}&zo=/${address}/+0.001km${options}/&page=${page ? page : '1'}&pagesize=${size ? size : '25'}`;
 
 express()
   .use('/static', express.static('public', {maxAge: '31d'}))
@@ -8,47 +20,49 @@ express()
   .listen(3000, log);
 
 function home(req, res) {
-  if (req.query.chatInput) {
-    res.redirect('/' + req.query.chatInput);
+  if (req.query.location) {
+    const options = req.query.outdoor ? `/${req.query.outdoor}` : '';
+    fetch(fundaEndpoint(req.query.type, req.query.location, options))
+      .then(data => data.json())
+      .then(json => makeHtml(json, mainHtml()))
+      .then(houses => callback(houses))
+      .catch(err => error(req, res, err));
   } else {
-    respond(
-      res,
-      `
-        <section class="message fundapi">
-          <div class="avatar"><img src="/static/assets/img/fundapi-avatar.svg" /></div>
-          <header><h1>Fundapi</h1></header>
-          <p>Welkom vul het onderstaande formulier in om een zoekopdracht te doen.</p>
-          <form>
-            <fieldset>
-              <legend>Locatie</legend>
-              <input type="text" name="location" placeholder="Locatie" autocomplete="off" />
-            </fieldset>
-            <fieldset>
-              <legend>Type wooning</legend>
-              <label><input type="radio" name="type" value="koop" />Koop</label>
-              <label><input type="radio" name="type" value="huur" />Huur</label>
-            </fieldset>
-            <fieldset>
-              <legend>Buiten ruimte</legend>
-              <label><input type="radio" name="outdoor" value="balkon" />Balkon</label>
-              <label><input type="radio" name="outdoor" value="dakterras" />Dakterras</label>
-              <label><input type="radio" name="outdoor" value="tuin" />Tuin</label>
-            </fieldset>
-            <input type="submit" value="send" />
-          </form>
-        </section>
-      `);
+    callback(mainHtml());
+  }
+
+  function callback(body) {
+    respond(res, body);
   }
 }
 
-function error(req, res) {
+function makeHtml(json, extraHtml) {
+  let houses = '';
+  json.Objects.forEach(object => {
+    houses += `
+      <section class="message house">
+        <div class="avatar"><img src="static/assets/img/fundapi-avatar.svg" /></div>
+        <header><h1>${object.Adres}</h1></header>
+        <p>
+          <img class="house-img" src="${object.FotoLarge}"/>
+          ${object.HuurprijsFormaat ? object.PrijsGeformatteerdTextHuur : object.PrijsGeformatteerdTextKoop}
+        </p>
+      </section>
+    `;
+  });
+  extraHtml ? houses += extraHtml : null;
+  return houses;
+}
+
+function error(req, res, err) {
+  console.warn(err); // eslint-disable-line no-console
   respond(
     res,
     `
-      <section class="message fundapi" style={{ background: blue }}>
+      <section class="message fundapi">
         <div class="avatar"><img src="/static/assets/img/fundapi-avatar.svg" /></div>
         <header><h1>404</h1></header>
-        <p>It seems u have stumbled upon a non-excisting page "${req.originalUrl}"</p>
+        <p>Oopsies something went wrong. If you think what you did is supposed to work please mail me at <a href="mailto:ian@icstewart.com">ian@icstewart.com</a></p>
         <p>browse back <a href="/">home</a> to get back on track</p>
       </section>
     `
