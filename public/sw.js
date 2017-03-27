9a7d73c8e0ec1c6050831d351dd23943cd3f6d0b
iancstewart <ian.c.stewart@icloud.com>
2017-03-27 19:47:04 +0200

    :sparkles: Served `offline` page when offline.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,18 @@
 /* eslint-env browser */
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('install', event => event.waitUntil(
+  caches.open('funda-v1-core')
+    .then(cache => cache.add('/offline/'))
+    .then(self.skipWaiting())
+));
 
 self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
+  event.respondWith(
+    fetch(event.request)
+      .catch(err => fetchOfflinePage()) //eslint-disable-line no-unused-vars
+  );
 });
+
+function fetchOfflinePage() {
+  return caches.open('funda-v1-core')
+    .then(cache => cache.match('/offline/'));
+}
