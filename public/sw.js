030f4ec959bb7b381ba57346f76937190f61622c
iancstewart <ian.c.stewart@icloud.com>
2017-03-27 19:39:14 +0200

    :sparkles: Added `offline` page.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
 /* eslint-env browser */
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
 });
