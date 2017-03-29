4d88e0b845c1d02ea12f86caccf10bccec9fc519
iancstewart <ian.c.stewart@icloud.com>
2017-03-29 11:18:26 +0200

    :sparkles: Use cached files.

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,29 @@
 /* eslint-env browser */
 self.addEventListener('install', event => event.waitUntil(
   caches.open('funda-v1-core')
-    .then(cache => cache.add('/offline/'))
+    .then(cache => cache.addAll([
+      '/offline/',
+      '/style.css',
+      '/reset.min.css',
+      '/assets/img/chat-background.jpg',
+      '/assets/img/logo.svg',
+      '/assets/img/fundapi-avatar.svg',
+      '/funda.js'
+    ]))
     .then(self.skipWaiting())
 ));
 
 self.addEventListener('fetch', function(event) {
+  const request = event.request;
   event.respondWith(
     fetch(event.request)
-      .catch(err => fetchOfflinePage()) //eslint-disable-line no-unused-vars
+      .catch(err => fetchCoreFile(request.url)) //eslint-disable-line no-unused-vars
+      .catch(err => fetchCoreFile('/offline/')) //eslint-disable-line no-unused-vars
   );
 });
 
-function fetchOfflinePage() {
+function fetchCoreFile(url) {
   return caches.open('funda-v1-core')
-    .then(cache => cache.match('/offline/'));
+    .then(cache => cache.match(url))
+    .then(response => response ? response : Promise.reject());
 }
